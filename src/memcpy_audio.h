a69cab8d0367a236d539a569b5a5c1ab5e7bc851
Newdigate <nicnewdigate@gmail.com>
2021-05-03 19:05:08 +0100

    delete memcpy_audio.cpp

--- src/memcpy_audio.h
+++ src/memcpy_audio.h
@@ -29,20 +29,26 @@
 
 #ifndef memcpy_audio_h_
 #define memcpy_audio_h_
-/*
-#ifdef __cplusplus
-extern "C" {
-#endif
- */
-void memcpy_tointerleaveLR(int16_t *dst, const int16_t *srcL, const int16_t *srcR);
-void memcpy_tointerleaveL(int16_t *dst, const int16_t *srcL);
-void memcpy_tointerleaveR(int16_t *dst, const int16_t *srcR);
-void memcpy_tointerleaveQuad(int16_t *dst, const int16_t *src1, const int16_t *src2, const int16_t *src3, const int16_t *src4);
-/*
-#ifdef __cplusplus
+
+void memcpy_tointerleaveLR(int16_t *dst, const int16_t *srcL, const int16_t *srcR){
+    for (int i=0; i< AUDIO_BLOCK_SAMPLES; i++) {
+        *dst++ = *srcL++;
+        *dst++ = *srcR++;
+    }
 }
-#endif
-*/
-
-
+void memcpy_tointerleaveL(int16_t *dst, const int16_t *srcL){
+    for (int i=0; i< AUDIO_BLOCK_SAMPLES; i++) {
+        *dst++ = *srcL++;
+        dst++;
+    }
+}
+void memcpy_tointerleaveR(int16_t *dst, const int16_t *srcR){
+    for (int i=0; i< AUDIO_BLOCK_SAMPLES; i++) {
+        dst++;
+        *dst++ = *srcR++;
+    }
+}
+void memcpy_tointerleaveQuad(int16_t *dst, const int16_t *src1, const int16_t *src2, const int16_t *src3, const int16_t *src4){
+    // TODO: this...
+}
 #endif
