a7e98bb5e851e872673ebc1da5c0463f950fd63a
Newdigate <nicnewdigate@gmail.com>
2021-05-03 18:04:10 +0100

    attempt to fix

--- src/memcpy_audio.h
+++ src/memcpy_audio.h
@@ -29,18 +29,20 @@
 
 #ifndef memcpy_audio_h_
 #define memcpy_audio_h_
-
+/*
 #ifdef __cplusplus
 extern "C" {
 #endif
+ */
 void memcpy_tointerleaveLR(int16_t *dst, const int16_t *srcL, const int16_t *srcR);
 void memcpy_tointerleaveL(int16_t *dst, const int16_t *srcL);
 void memcpy_tointerleaveR(int16_t *dst, const int16_t *srcR);
-void memcpy_tointerleaveQuad(int16_t *dst, const int16_t *src1, const int16_t *src2,
-	const int16_t *src3, const int16_t *src4);
+void memcpy_tointerleaveQuad(int16_t *dst, const int16_t *src1, const int16_t *src2, const int16_t *src3, const int16_t *src4);
+/*
 #ifdef __cplusplus
 }
 #endif
+*/
 
 
 #endif
