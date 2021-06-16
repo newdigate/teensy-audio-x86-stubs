256298402277446a58a19c3849742addc0763e34
Newdigate <nicnewdigate@gmail.com>
2021-06-16 17:33:41 +0100

    add mixer example

---
+++ extras/soundio/examples/mixer/AudioSampleKick.h
@@ -0,0 +1,4 @@
+// https://raw.githubusercontent.com/PaulStoffregen/Audio/master/examples/SamplePlayer/AudioSampleKick.h
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleKick[2561];
