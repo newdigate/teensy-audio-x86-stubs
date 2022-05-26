34842da102c1f4a572d4c8eb246ce31b12078351
Newdigate <nicnewdigate@gmail.com>
2022-05-26 17:26:54 +0100

    split memcpy_audio.h into two files, i.e. .cpp; fix linking error in downstream projects

--- src/memcpy_audio.cpp
+++ src/memcpy_audio.cpp
@@ -26,14 +26,26 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+#include "memcpy_audio.h"
 
-#ifndef memcpy_audio_h_
-#define memcpy_audio_h_
-
-#include "Audio.h"
-
-void memcpy_tointerleaveLR(int16_t *dst, const int16_t *srcL, const int16_t *srcR);
-void memcpy_tointerleaveL(int16_t *dst, const int16_t *srcL);
-void memcpy_tointerleaveR(int16_t *dst, const int16_t *srcR);
-void memcpy_tointerleaveQuad(int16_t *dst, const int16_t *src1, const int16_t *src2, const int16_t *src3, const int16_t *src4);
-#endif
+void memcpy_tointerleaveLR(int16_t *dst, const int16_t *srcL, const int16_t *srcR){
+    for (int i=0; i< AUDIO_BLOCK_SAMPLES; i++) {
+        *dst++ = *srcL++;
+        *dst++ = *srcR++;
+    }
+}
+void memcpy_tointerleaveL(int16_t *dst, const int16_t *srcL){
+    for (int i=0; i< AUDIO_BLOCK_SAMPLES; i++) {
+        *dst++ = *srcL++;
+        dst++;
+    }
+}
+void memcpy_tointerleaveR(int16_t *dst, const int16_t *srcR){
+    for (int i=0; i< AUDIO_BLOCK_SAMPLES; i++) {
+        dst++;
+        *dst++ = *srcR++;
+    }
+}
+void memcpy_tointerleaveQuad(int16_t *dst, const int16_t *src1, const int16_t *src2, const int16_t *src3, const int16_t *src4){
+    // TODO: this...
+}
